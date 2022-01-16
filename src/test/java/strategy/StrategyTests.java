5b7607bbbe181f703172f3d7eba86a0b1c5afce7
sebrown0 <thebrownfamily@live.co.uk>
2022-01-16 11:32:05 +0100

    First commit.

---
+++ src/test/java/strategy/StrategyTests.java
@@ -0,0 +1,14 @@
+package strategy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StrategyTests {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
